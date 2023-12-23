b00cadaf68dfcd161a297022bbccdcdc03383e55
Edison Lee <edisonlee@edisonlee55.com>
2023-12-24 06:18:13 +0800

    v0.1.0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.edisonlee55.com',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    }
+}
 
 module.exports = nextConfig
