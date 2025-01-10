86038f8ea394f4a42ed7f5297519afb05818ffef
yuna0x0 <yuna@yuna0x0.com>
2025-01-10 17:14:50 +0800

    Add site title
    Upgrade dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import localFont from 'next/font/local';
+import type { Metadata } from 'next';
 import './globals.css'
 
 const Avenir = localFont({ src: './fonts/Avenir.woff2' });
+
+export const metadata: Metadata = {
+  title: 'yuna0x0 Domain Parking'
+}
 
 export default function RootLayout({
   children,
